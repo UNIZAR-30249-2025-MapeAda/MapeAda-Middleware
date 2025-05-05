59a2b028e14beee65ddcf15218808eaee54b1160
Lilai <840091@unizar.es>
2025-05-05 23:03:36 +0200

    fixed errores
    Co-authored-by: Miguel Granel <miguel93041@users.noreply.github.com>

--- Extensions/HttpResponseMessageExtensions.cs
+++ Extensions/HttpResponseMessageExtensions.cs
@@ -1,16 +1,41 @@
+using System.Net.Http;
+using System.Net.Http.Json;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
-namespace MapeAda_Middleware.Extensions;
-
-public static class HttpResponseMessageExtensions
+namespace MapeAda_Middleware.Extensions
 {
-    public static async Task<IResult> ToProblem(this HttpResponseMessage response)
+    public static class HttpResponseMessageExtensions
     {
-        ValidationProblemDetails? problemDetails = await response.Content.ReadFromJsonAsync<ValidationProblemDetails>();
-        return Results.Problem(
-            detail: string.Join("; ", problemDetails?.Errors.SelectMany(e => e.Value) ?? []),
-            statusCode: problemDetails?.Status ?? (int)response.StatusCode,
-            title: problemDetails?.Title,
-            type: problemDetails?.Type);
+        public static async Task<IResult> ToProblem(this HttpResponseMessage response)
+        {
+            // Intentamos leer un ValidationProblemDetails
+            var vpd = await response.Content
+                .ReadFromJsonAsync<ValidationProblemDetails>();
+
+            if (vpd is not null)
+            {
+                // Devolvemos exactamente los mismos errores y status
+                return Results.ValidationProblem(
+                    vpd.Errors,
+                    title: vpd.Title,
+                    statusCode: vpd.Status ?? StatusCodes.Status400BadRequest,
+                    type: vpd.Type,
+                    instance: vpd.Instance
+                );
+            }
+
+            // Si no es un ValidationProblemDetails… caemos en Problem normal
+            var pd = await response.Content
+                .ReadFromJsonAsync<ProblemDetails>();
+
+            return Results.Problem(
+                detail: pd?.Detail,
+                statusCode: pd?.Status ?? (int)response.StatusCode,
+                title: pd?.Title,
+                type: pd?.Type,
+                instance: pd?.Instance
+            );
+        }
     }
 }
